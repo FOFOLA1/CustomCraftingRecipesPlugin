f51d61d2764eebdb513e264784c994b8024e8c64
Filip Řeháček <filiprehacek4@gmail.com>
2025-04-14 21:17:34 +0200

    todos deleted testing recipe

--- src/main/java/me/fofola1/customCraftingRecipesPlugin/CustomCraftingRecipesPlugin.java
+++ src/main/java/me/fofola1/customCraftingRecipesPlugin/CustomCraftingRecipesPlugin.java
@@ -17,6 +17,21 @@ import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
+// TODO Smazat nepotřebný importy
+// TODO Smazat test commandy (i z plugin.yml), classy, outputy
+// TODO Lepší vypisy do logu, třeba trochu formátování atd.
+// TODO (opt.) Editor
+// TODO Logika pro auto discover
+// TODO vymyslet material pro auto discover v menu
+// TODO (opt.) celý to přepsat do pořádnýho OOP
+// TODO (opt.) lépe promyslet názvy a obsah tříd DataStorage, MenuData, Preloaded, Utils
+// TODO Command na discovernutí recipu
+// TODO Připsat další často opakované části kódu do metody do Recipes
+// TODO Projít code inspektor a projít warningy
+// TODO *************** UDĚLAT DOKUMENTACI ***************
+
+
+
 public final class CustomCraftingRecipesPlugin extends JavaPlugin {
 
     private static CustomCraftingRecipesPlugin instance;
@@ -32,43 +47,6 @@ public final class CustomCraftingRecipesPlugin extends JavaPlugin {
 
         Lang.load();
         Recipes.LoadAllRecipes();
-
-
-        ItemStack item = new ItemStack(Material.DIAMOND_SWORD);
-
-        // The meta of the diamond sword where we can change the name, and properties of the item.
-        ItemMeta meta = item.getItemMeta();
-
-        // We will initialise the next variable after changing the properties of the sword
-
-        // This sets the name of the item.
-        meta.setDisplayName(ChatColor.GREEN + "" + ChatColor.BOLD + "Emerald Sword");
-
-        // Set the meta of the sword to the edited meta.
-        item.setItemMeta(meta);
-
-        // Add the custom enchantment to make the emerald sword special
-        // In this case, we're adding the permission that modifies the damage value on level 5
-        // Level 5 is represented by the second parameter. You can change this to anything compatible with a sword
-        // create a NamespacedKey for your recipe
-        key = new NamespacedKey(this, "emerald_sword");
-
-        // Create our custom recipe variable
-        ShapedRecipe recipe = new ShapedRecipe(key, item);
-
-        // Here we will set the places. E and S can represent anything, and the letters can be anything. Beware; this is case sensitive.
-        recipe.shape("E", "E", "S");
-
-        // Set what the letters represent.
-        // E = Emerald, S = Stick
-        recipe.setIngredient('E', Material.EMERALD);
-        recipe.setIngredient('S', Material.STICK);
-
-        // Finally, add the recipe to the bukkit recipes
-        Bukkit.addRecipe(recipe);
-
-
-
 
 
         getCommand("removerecipe").setExecutor(new RemoveRecipeCommand()); // TEST
