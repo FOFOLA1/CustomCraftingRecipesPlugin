6b9e4d2f54e43df1a655c95ce8047953df49fc23
Filip Řeháček <filiprehacek4@gmail.com>
2025-05-17 17:34:55 +0200

    propper plugin disabling and starting

--- src/main/java/me/fofola1/CustomRecipes/CustomRecipes.java
+++ src/main/java/me/fofola1/CustomRecipes/CustomRecipes.java
@@ -9,14 +9,14 @@ import me.fofola1.CustomRecipes.Menus.Holders.RecipesBookMenuHolder;
 import me.fofola1.CustomRecipes.Utils.MenuData;
 import me.fofola1.CustomRecipes.Utils.Recipes;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.Material;
-import org.bukkit.NamespacedKey;
-import org.bukkit.enchantments.Enchantment;
-import org.bukkit.inventory.ItemStack;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.HandlerList;
 import org.bukkit.inventory.ShapedRecipe;
-import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.ArrayList;
+import java.util.UUID;
 
 // TODO Smazat nepotřebný importy
 // TODO Smazat test commandy (i z plugin.yml), classy, outputy
@@ -27,9 +27,10 @@ import org.bukkit.plugin.java.JavaPlugin;
 // TODO (opt.) celý to přepsat do pořádnýho OOP
 // TODO (opt.) lépe promyslet názvy a obsah tříd DataStorage, MenuData, Preloaded, Utils
 // TODO Command na discovernutí recipu
-// TODO Připsat další často opakované části kódu do metody do Recipes
+// TODO Připsat další často opakované části kódu do metody do Recipes X
 // TODO Projít code inspektor a projít warningy
-// TODO *************** UDĚLAT DOKUMENTACI ***************
+// TODO *************** UDĚLAT DOKUMENTACI *************** X
+// TODO dodělat metody v CustomRecipesCommand
 
 
 
@@ -45,6 +46,9 @@ public final class CustomRecipes extends JavaPlugin {
         getConfig().options().copyDefaults();
         saveDefaultConfig();
 
+        Recipes.AutoDiscoverRecipes = new ArrayList<ShapedRecipe>();
+        Recipes.ManualDiscoverRecipes = new ArrayList<ShapedRecipe>();
+
         Lang.load();
         Recipes.LoadAllRecipes();
 
@@ -56,20 +60,90 @@ public final class CustomRecipes extends JavaPlugin {
         getCommand("getnewenfile").setExecutor(new GetNewEnFile());  // TEST
         getCommand("getlangstring").setExecutor(new GetLangStringCommand());  // TEST
         getCommand("getrecipeitems").setExecutor(new GetRecipeItemsCommand()); // TEST
-        getCommand("customrecipe").setExecutor(new CustomCraftingCommand());
+        getCommand("customrecipe").setExecutor(new CustomRecipesCommand());
+        getCommand("customrecipe").setTabCompleter(new CustomRecipesTabCompleter());
+
 
         getServer().getPluginManager().registerEvents(new MenuClickListener(), this);
         getServer().getPluginManager().registerEvents(new MenuCloseListener(), this);
         getServer().getPluginManager().registerEvents(new MenuDragListener(), this);
         getServer().getPluginManager().registerEvents(new MessageListener(), this);
+        getServer().getPluginManager().registerEvents(new PlayerJoinListener(), this);
+        getServer().getPluginManager().registerEvents(new RecipeCraftListener(), this);
 
         LogInfo("PLUGIN STARTED");
     }
 
     @Override
     public void onDisable() {
-        Bukkit.removeRecipe(key);
+        Bukkit.getScheduler().cancelTasks(this);
+        //HandlerList.unregisterAll();
+        for (ShapedRecipe recipe: Recipes.AutoDiscoverRecipes) {
+            Bukkit.removeRecipe(recipe.getKey());
+        }
+        for (ShapedRecipe recipe: Recipes.ManualDiscoverRecipes) {
+            Bukkit.removeRecipe(recipe.getKey());
+        }
+        Recipes.ManualDiscoverRecipes = null;
+        Recipes.AutoDiscoverRecipes = null;
+
+        for (UUID uuid : MenuData.getUUIDList()) {
+            Player p = Bukkit.getPlayer(uuid);
+            if (!p.isOnline()) continue;
+            if (
+                    p.getOpenInventory().getTopInventory().getHolder() instanceof CraftingRecipeMenuHolder ||
+                    p.getOpenInventory().getTopInventory().getHolder() instanceof MainMenuHolder ||
+                    p.getOpenInventory().getTopInventory().getHolder() instanceof RecipesBookMenuHolder
+            ) {
+                p.closeInventory();
+                p.sendMessage("Plugin is currently disabling, try again later!");
+            }
+        }
+
+        saveConfig();
+
+
         LogInfo("PLUGIN STOPPED");
+    }
+
+    public void reloadPlugin() {
+        Bukkit.getScheduler().cancelTasks(this);
+
+        for (ShapedRecipe recipe: Recipes.AutoDiscoverRecipes) {
+            Bukkit.removeRecipe(recipe.getKey());
+        }
+        for (ShapedRecipe recipe: Recipes.ManualDiscoverRecipes) {
+            Bukkit.removeRecipe(recipe.getKey());
+        }
+        Recipes.ManualDiscoverRecipes = null;
+        Recipes.AutoDiscoverRecipes = null;
+        Lang.unload();
+
+        for (UUID uuid : MenuData.getUUIDList()) {
+            Player p = Bukkit.getPlayer(uuid);
+            if (!p.isOnline()) continue;
+            if (
+                    p.getOpenInventory().getTopInventory().getHolder() instanceof CraftingRecipeMenuHolder ||
+                            p.getOpenInventory().getTopInventory().getHolder() instanceof MainMenuHolder ||
+                            p.getOpenInventory().getTopInventory().getHolder() instanceof RecipesBookMenuHolder
+            ) {
+                p.closeInventory();
+                p.sendMessage("Plugin is currently disabling, try again later!");
+            }
+        }
+        saveConfig();
+        LogInfo("PLUGIN STOPPED");
+
+
+        getConfig().options().copyDefaults();
+        saveDefaultConfig();
+
+        Recipes.AutoDiscoverRecipes = new ArrayList<ShapedRecipe>();
+        Recipes.ManualDiscoverRecipes = new ArrayList<ShapedRecipe>();
+
+        Lang.load();
+        Recipes.LoadAllRecipes();
+        LogInfo("PLUGIN STARTED");
     }
 
 
